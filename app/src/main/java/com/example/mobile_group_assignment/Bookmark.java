db844f780e5dc604315b45c73f4338cd06d6149d
BRB-0522 <tmdfldmlqkfldk>
2023-06-02 05:16:54 +1200

    version1

---
+++ app/src/main/java/com/example/mobile_group_assignment/Bookmark.java
@@ -0,0 +1,16 @@
+package com.example.mobile_group_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bookmark extends AppCompatActivity {
+
+    //Main page. Bookshelf
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookmark);
+    }
+}
